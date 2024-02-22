dc2960358c7bae20d0adc05a951482ab4b6da820
Nathan <nathanferreira00@hotmail.com>
2024-02-22 15:30:27 -0300

    chore: eslint config with drizzle plugin

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,18 +1,17 @@
-import { uuid, text, timestamp, pgTable } from "drizzle-orm/pg-core";
+import { uuid, text, timestamp, pgTable } from 'drizzle-orm/pg-core'
 import { users } from './users'
-import { relations } from "drizzle-orm";
+import { relations } from 'drizzle-orm'
 
-
-export const restaurants = pgTable("restaurants", {
-  id: uuid("id").primaryKey(),
-  name: text("name").notNull(),
-  description: text("description"),
+export const restaurants = pgTable('restaurants', {
+  id: uuid('id').primaryKey(),
+  name: text('name').notNull(),
+  description: text('description'),
   managerId: uuid('manager_id').references(() => users.id, {
     onDelete: 'set null',
   }),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").notNull().defaultNow(),
-});
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
 
 // a restaurant has one manager
 export const restaurantsRelations = relations(restaurants, ({ one }) => {
@@ -20,7 +19,7 @@ export const restaurantsRelations = relations(restaurants, ({ one }) => {
     manager: one(users, {
       fields: [restaurants.managerId],
       references: [users.id],
-      relationName: 'restaurant_name'
-    })
+      relationName: 'restaurant_name',
+    }),
   }
 })
