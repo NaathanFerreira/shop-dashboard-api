65058d445a243571b355ea7db44a6856f761dba0
Nathan <nathanferreira00@hotmail.com>
2024-02-27 15:18:59 -0300

    feat: get profile route

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -16,7 +16,7 @@ export const auth = new Elysia()
     }),
   )
   .use(cookie())
-  .derive(({ jwt, setCookie }) => {
+  .derive(({ jwt, setCookie, cookie }) => {
     return {
       signUser: async (payload: Static<typeof jwtPayload>) => {
         const token = await jwt.sign(payload)
@@ -26,6 +26,21 @@ export const auth = new Elysia()
           maxAge: 60 * 60 * 24 * 7, // 7 days
           path: '/',
         })
+      },
+
+      getCurrentUser: async () => {
+        const authCookie = cookie.auth
+
+        const payload = await jwt.verify(authCookie)
+
+        if (!payload) {
+          throw new Error('Unauthorized.')
+        }
+
+        return {
+          userId: payload.sub,
+          restaurantId: payload.restaurantId,
+        }
       },
     }
   })
